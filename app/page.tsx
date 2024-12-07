47d50662ef3c2859ae4b7cf9d3c5637618793050
mhmd-aashik <aashikdevelop@gmail.com>
2024-12-07 23:43:47 +0530

    chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Sidebar from "@/components/Sidebar";
+import { ChartEmploee } from "@/components/ChartEmploee";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+const Home = () => {
+  return (
+    <div className="flex w-full">
+      <div>
+        <Sidebar />
+      </div>
+      <div className="flex items-center max-w-6xl mx-auto justify-center flex-1">
+        <div>
+          <h1 className="text-4xl font-bold text-center">
+            {/* Welcome to the Home Page */}
+          </h1>
+          <div className="flex gap-5 flex-wrap items-center justify-center">
+            <ChartEmploee name="Emploee" />
+            <ChartEmploee name="Machinary" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default HomePage;
+export default Home;
---
+++ components/ChartEmploee.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { Pie, PieChart } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+const chartData = [
+  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
+  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+  { browser: "other", visitors: 90, fill: "var(--color-other)" },
+];
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "hsl(var(--chart-1))",
+  },
+  safari: {
+    label: "Safari",
+    color: "hsl(var(--chart-2))",
+  },
+  firefox: {
+    label: "Firefox",
+    color: "hsl(var(--chart-3))",
+  },
+  edge: {
+    label: "Edge",
+    color: "hsl(var(--chart-4))",
+  },
+  other: {
+    label: "Other",
+    color: "hsl(var(--chart-5))",
+  },
+} satisfies ChartConfig;
+
+interface ChartEmploeeProps {
+  name: string;
+}
+
+export function ChartEmploee({ name }: ChartEmploeeProps) {
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="items-center pb-0">
+        <CardTitle>{name} Chart</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square h-[450px] pb-0 [&_.recharts-pie-label-text]:fill-foreground"
+        >
+          <PieChart>
+            <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+            <Pie data={chartData} dataKey="visitors" label nameKey="browser" />
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
