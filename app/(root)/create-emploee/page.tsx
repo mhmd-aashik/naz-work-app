c7f35e0e17fb456eeeb2c023a7aadf389aa7788e
mhmd-aashik <aashikdevelop@gmail.com>
2024-12-08 17:10:17 +0530

    donnn

--- app/(root)/create-emploee/page.tsx
+++ app/(root)/create-emploee/page.tsx
@@ -4,7 +4,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-// import { toast } from "@/components/hooks/use-toast";
 import { Button } from "@/components/ui/button";
 import {
   Form,
